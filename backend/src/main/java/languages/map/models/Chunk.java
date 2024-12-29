13b41556d63e55a3df5576028e3324528a4739d8
Mykyta <nktpedko@gmail.com>
2024-12-29 21:59:24 +0200

    sum

--- backend/src/main/java/languages/map/models/Chunk.java
+++ backend/src/main/java/languages/map/models/Chunk.java
@@ -1,11 +1,9 @@
 package languages.map.models;
 
-import com.mongodb.client.model.geojson.Point;
 import lombok.Data;
 import org.bson.types.Binary;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.geo.GeoJson;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.mapping.Document;
 
